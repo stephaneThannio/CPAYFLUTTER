5b4fcfe56356bd8caab8cdbca1d54a69c85eafd3
hery <bienaimehery1@gmail.com>
2023-08-09 09:13:02 +0300

    lib\pages\depotpage.dart

--- lib/pages/depotpage.dart
+++ lib/pages/depotpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class PageDepot extends StatefulWidget {
   const PageDepot({super.key});
@@ -7,7 +8,71 @@ class PageDepot extends StatefulWidget {
   State<PageDepot> createState() => _PageDepotState();
 }
 
-class _PageDepotState extends State<PageDepot> {
+class _PageDepotState extends State<PageDepot>
+    with SingleTickerProviderStateMixin {
+  //late AnimationController _animationController;
+  //late AnimationController _animationController2;
+  String paymenmode = "Mvola";
+  Color mvolaContentColor = Colors.green;
+  Color banktransferContentcolor = Colors.transparent;
+
+  // setAnimation() {
+  //   if (paymenmode == "Mvola") {
+  //     _animationController.forward();
+  //     //_animationController2.reverse();
+  //   } else if (paymenmode == "banc transfert") {
+  //     _animationController.reverse();
+  //     //_animationController2.forward();
+  //   }
+  // }
+
+  onBouttonMvolatap() {
+    setState(() {
+      paymenmode = "Mvola";
+    });
+    setColor();
+  }
+
+  onButtonBanktransfertap() {
+    setState(() {
+      paymenmode = "banc transfert";
+    });
+    setColor();
+  }
+
+  setColor() {
+    if (paymenmode == "Mvola") {
+      setState(() {
+        mvolaContentColor = Colors.green;
+        banktransferContentcolor = Colors.transparent;
+      });
+    } else if (paymenmode == "banc transfert") {
+      setState(() {
+        mvolaContentColor = Colors.transparent;
+        banktransferContentcolor = Colors.green;
+      });
+    }
+  }
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   //_animationController = AnimationController(
+  //       //vsync: this, duration: const Duration(milliseconds: 200));
+  //   // _animationController2 = AnimationController(
+  //   //     vsync: this, duration: const Duration(milliseconds: 300));
+  //   //setAnimation();
+  // }
+
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  //   //_animationController.dispose();
+  //   //_animationController2.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
     double screenwidth = MediaQuery.of(context).size.width;
@@ -163,21 +228,57 @@ class _PageDepotState extends State<PageDepot> {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            SizedBox(
-                              height: 100,
-                              width: 100,
-                              child: Card(
-                                elevation: 6,
-                                child: Image.asset('lib/photos/mvola.webp'),
+                            GestureDetector(
+                              onTap: () {
+                                //setAnimation();
+                                onBouttonMvolatap();
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: mvolaContentColor,
+                                    borderRadius: BorderRadius.circular(10)),
+                                height: 100,
+                                width: 100,
+                                child: paymenmode == "Mvola"
+                                    ? Card(
+                                        elevation: 6,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Image.asset(
+                                              'lib/photos/mvola.webp'),
+                                        ),
+                                      ).animate().shake()
+                                    : Card(
+                                        elevation: 6,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Image.asset(
+                                              'lib/photos/mvola.webp'),
+                                        ),
+                                      ),
                               ),
                             ),
-                            SizedBox(
-                              height: 100,
-                              width: 100,
-                              child: Card(
-                                elevation: 6,
-                                child:
-                                    Image.asset('lib/photos/banktransfert.png'),
+                            GestureDetector(
+                              onTap: () {
+                                onButtonBanktransfertap();
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: banktransferContentcolor,
+                                    borderRadius: BorderRadius.circular(10)),
+                                height: 100,
+                                width: 100,
+                                child: paymenmode == "banc transfert"
+                                    ? Card(
+                                        elevation: 6,
+                                        child: Image.asset(
+                                            'lib/photos/banktransfert.png'),
+                                      ).animate().shake()
+                                    : Card(
+                                        elevation: 6,
+                                        child: Image.asset(
+                                            'lib/photos/banktransfert.png'),
+                                      ),
                               ),
                             ),
                           ],
