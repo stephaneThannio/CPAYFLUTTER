ec2e904ceda62715fad236e39ec9156e636578de
hery <bienaimehery1@gmail.com>
2023-08-22 23:15:56 +0300

    lib\items\itemsTab\retraititem.dart lib\items\itemsTab\virementitem.dart

---
+++ lib/items/itemsTab/virementitem.dart
@@ -0,0 +1,75 @@
+//import 'package:cpay/models/depottransaction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class VirementItem extends StatelessWidget {
+  const VirementItem(
+      {super.key,
+      required this.status,
+      required this.date,
+      required this.montant,
+      required this.info});
+  final String status;
+  final String date;
+  final String montant;
+  final String info;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: screenwidth * 0.8,
+          child: Card(
+            elevation: 2,
+            color: Colors.white,
+            child: ListTile(
+              title: Text(
+                status,
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xFF6334A9),
+                  fontFamily: 'PlusJakartaSans',
+                ),
+              ),
+              subtitle: Text(
+                date,
+                style: TextStyle(
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey,
+                  fontFamily: 'PlusJakartaSans',
+                ),
+              ),
+              trailing: Text(
+                "$montant MGA",
+                style: TextStyle(
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xFF6334A9),
+                  fontFamily: 'PlusJakartaSans',
+                ),
+              ),
+              leading: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.sp),
+                    color: const Color(0xFF6334A9).withOpacity(0.5)),
+                child: IconButton(
+                  icon: Image(
+                      width: 30.sp,
+                      height: 30.sp,
+                      image: const AssetImage('lib/photos/mvola.webp')
+                      // : const AssetImage('lib/photos/banktransfert.png'),
+                      ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
